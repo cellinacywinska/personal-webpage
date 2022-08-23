403d469fe54ea063ec83f0435d1e478c2a795ffd
Celinka Cywińska <celinkacywinska@Celinkas-MacBook-Air.local>
2022-08-22 19:03:06 -0700

    Fix padding

--- src/Experience.js
+++ src/Experience.js
@@ -6,14 +6,13 @@ import image from './mlh-logo.png';
 
 export default () => {
     return (
-    <div style={{width:"100%", height:"100%", top:"35%", transform:"translate(0,-20%)",
+    <div style={{width:"100%", height:"80%", top:"35%", transform:"translate(0,-20%)",
     position: "absolute"}}>
          <div style={{textAlign:"center"}}>
              <h3>
             Open Source track intern in the G-Research DevOps team.<br></br>
             Working on a Github Runners Open Source Project.<br></br>
             Utilizing Github Actions, Pulumi, AWS, and GCP. 
-         
             </h3>
          </div>
          <a href="https://fellowship.mlh.io/" target="blank">
